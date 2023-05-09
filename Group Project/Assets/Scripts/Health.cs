bd519a2cbf72e1cd0211d4a996b4d65f0e92c2f9
AndresG05 <solareclipse3388@gmail.com>
2023-05-09 09:23:01 -0700

    PlayerAttackAnimation

--- Group Project/Assets/Scripts/Health.cs
+++ Group Project/Assets/Scripts/Health.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public int health;
+    [SerializedField] private int health = 5;
+
+    private int MAX_HEALTH = 10;
     // Start is called before the first frame update
-    void Start()
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Damage(int amount)
     {
-        
+        if(amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+        }
+
+        this.health -= amount;
+
+        if(health <= 0)
+        {
+
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+        }
+
+        bool woudlBeOverMaxHealth = health + amount > MAX_HEALTH;
+
+        if(woudlBeOverMaxHealth)
+        {
+            this.health = MAX_HEALTH;
+        }
+        else
+        {
+            this.health += amount;
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("I am DEAD!");
+        Destroy(gameObject);
     }
 }
