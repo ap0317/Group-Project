b935a03f4d9ea7682da812fe42cfb9e08f7a44b2
AndresG05 <solareclipse3388@gmail.com>
2023-05-23 08:39:38 -0700

    enemy is done but need to do health

---
+++ Group Project/Assets/Scripts/enemyHealth.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+    public int Health = 3;
+    
+    if()
+}
--- Group Project/Assets/Scripts/enemyPatrol.cs
+++ Group Project/Assets/Scripts/enemyPatrol.cs
@@ -19,13 +19,28 @@ public class enemyPatrol : MonoBehaviour
         {
             anim.SetBool("isRunning", true);
             transform.Translate(2 * Time.deltaTime * speed, 0, 0);
-            transform.localScale = new Vector2(2, 2);
+            transform.localScale = new Vector2(5, 5);
         }
         else
         {
             anim.SetBool("isRunning", true);
             transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
-            transform.localScale = new Vector2(-2, 2);
+            transform.localScale = new Vector2(-5, 5);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig)
+    {
+        if (trig.gameObject.CompareTag("turn"))
+        {
+            if (MoveRight)
+            {
+                MoveRight = false;
+            }
+            else
+            {
+                MoveRight = true;
+            }
         }
     }
 }
